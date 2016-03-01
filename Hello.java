eef59ac587dd4a5386b74ce7fa7c1166d258bf30
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-03-01 15:29:46 -0700

    began intro to java curriculum

---
+++ Hello.java
@@ -0,0 +1,12 @@
+//deaclare a class named Hello
+public class Hello {
+
+//create a method called main
+//it takes in a parameter called args
+//args is an array of strings
+  public static void main(String[] args){
+    //print the first arg and the second arg
+    System.out.println(args[0] + " " + args[1]);
+  }
+
+}
---
+++ Ops.java
@@ -0,0 +1,23 @@
+//define a class called Ops
+public class Ops {
+
+  //define a method called sum
+  //this method returns an int
+  //it has a parameter named x that is of type int
+  //it has a parameter named y that is of type int
+  public static int sum(int x, int y) {
+    return x + y;
+  }
+
+  //main gets executed when we run this class with the java command
+  public static void main(String [] args){
+    //invoke sum, passing in 1 and 2 as arguemnts and print the results
+    System.out.println( sum(1, 2) );
+    System.out.println( multiply(1, 2, 3) );
+
+  }
+
+  public static int multiply(int x, int y, int z) {
+    return x*y*z;
+  }
+}
